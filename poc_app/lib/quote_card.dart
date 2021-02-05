899e2de5cdd09b406ec7dc7fd43e59d1eb87bdbf
Daniel Iversen <daiverse@stud.ntnu.no>
2021-02-05 13:31:25 +0100

    Delete
    You can delete quotes

--- poc_app/lib/quote_card.dart
+++ poc_app/lib/quote_card.dart
@@ -4,7 +4,8 @@ import 'quote.dart';
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
 
 
   @override
@@ -31,6 +32,12 @@ class QuoteCard extends StatelessWidget {
                 color: Colors.grey[600],
               ),
             ),
+            SizedBox(height: 6.0),
+            FlatButton.icon(
+              onPressed: delete,
+              label: Text("delete quote"),
+              icon: Icon(Icons.delete),
+            )
           ],
         ),
       ),
