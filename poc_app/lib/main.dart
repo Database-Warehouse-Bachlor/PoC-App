85a91b0b2d487d6ec73c86056b6e9e6b871a678b
Sverre Lofthus <sverrlof@stud.ntnu.no>
2021-02-09 11:43:19 +0100

    added route for login

--- poc_app/lib/main.dart
+++ poc_app/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'Home.dart';
+import 'package:poc_app/services/login.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: "/login",
     routes: {
-      "/": (context) => Home()
+      "/": (context) => Home(),
+      "/login": (context) => Login()
     },
   ));
 }
