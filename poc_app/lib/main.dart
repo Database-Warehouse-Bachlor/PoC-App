3a7577ab8ba74a09a56df5909d44a220a1d5cdc8
Sverre Lofthus <sverrlof@stud.ntnu.no>
2021-02-09 09:41:15 +0100

    added dependencies

--- poc_app/lib/main.dart
+++ poc_app/lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: "/",
+    initialRoute: "/login",
     routes: {
       "/": (context) => Loading(),
+      "/login": (context) => Login(),
       "/home": (context) => Home(),
       "/tennants": (context) => Tennants(),
       "/log": (context) => Log()
